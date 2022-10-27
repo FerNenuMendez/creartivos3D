27810606a3f23ac36507c50fd2352efb0ff75f90
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2022-10-27 02:14:04 -0300

    PreEntrega1+Mendez

---
+++ src/components/Navbar/CartWidget/CartWidget.js
@@ -0,0 +1,9 @@
+import img from './logo.png'
+import './CartWidget.css'
+const CartWidget = () => {
+  return (
+    <img src={img} alt="Logo" className="Logo" />
+  )
+}
+
+export default CartWidget
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        <nav className="Navbar">
+            <h1 className="Navbar__h1">CREARTIVOS 3D</h1>
+            <ul className="Navbar__ul">
+                <li className="Navbar__ul__li">
+                    Nosotros
+                </li>
+                <li className="Navbar__ul__li">
+                    Mates
+                </li>
+                <li className="Navbar__ul__li">
+                    Contacto
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
