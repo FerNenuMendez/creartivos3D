6e961b3ed306489d637a5eda267b0d384555604d
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2022-11-04 00:30:04 -0300

    pre-visualizado de img

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,5 +1,6 @@
 import data from '../../data/catalogomates'
 import item from './item.css'
+import img from '../../assets/mates/bat1.JPG'
 
 const Item = () => {
     
@@ -7,6 +8,7 @@ const Item = () => {
     const mate = data.map((producto)=>{
         return(
         <div key={producto.id} className="item__container">
+            <img src= {img}  alt="Foto Mate" className="item__container__img"/>
             <h2 className="item__container__h2">Mate {producto.nombre}</h2>
             <h3 className="item__container__h3">Linea {producto.linea}</h3>
             <h3 className="item__container__h3">Precio ${producto.precio}</h3>
