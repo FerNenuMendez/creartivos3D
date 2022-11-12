38e3c936234c83e4af3c4ee96d2999240e92fab1
FerNenuMendez <fernando.modesto.mendez@gmail.com>
2022-11-12 14:49:33 -0300

    Agregado de react-icons

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,17 +1,18 @@
-import img from '../../assets/carrito.png'
+
+import { BiCartAlt } from "react-icons/bi";
 import './CartWidget.css'
-const num=document.getElementById('contador')
+// const num=document.getElementById('contador')
 
 const CartWidget = ({contador}) => {
-  if (contador===0){
-     num.setAttribute('class','logo__contador__escondido')
-  } else if(contador>0){
-     num.setAttribute('class','logo__contador')
-  }
+  // if (contador===0){
+  //    num.setAttribute('class','logo__contador__escondido')
+  // } else if(contador>0){
+  //    num.setAttribute('class','logo__contador')
+  // }
   
   return (
     <div className="carrito">
-      <img src={img} alt="Carrito de Compras" className="logo" />
+      <BiCartAlt className="logo"/>
       <p id='contador' className="logo__contador">{contador}</p>
     </div>
   )
